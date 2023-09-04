f65a333756a939dc18facd6daae2f41f38d083ef
Snehashis Lenka <snehashislenka@Snehashiss-MacBook-Pro.local>
2023-09-03 20:00:48 -0400

    updated project section

--- src/components/body/projects/Projects.js
+++ src/components/body/projects/Projects.js
@@ -6,6 +6,168 @@ const Projects = () => {
       <div className="section-heading">
         <h1 className="work-ex-heading">PROJECTS</h1>
       </div>
+      {/** new project block */}
+      <div className="experience-content-block">
+        <div className="experience-content-left">
+          <div className="experience-heading d-none d-sm-block">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/SnehashisWalk/gdrive-pdfs-summarizer"
+            >
+              <h1 style={{ textTransform: "uppercase" }}>
+              GDrive PDF’s Summarizer | Python | LangChain | OpenAI | Pinecone | Streamlit UI
+              </h1>
+            </a>
+          </div>
+          {/* projects heading for sm devices */}
+          <div className="exp-header sm-toggle">
+            <div className="exp-header-left">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/SnehashisWalk/gdrive-pdfs-summarizer"
+              >
+                <h1 style={{ textTransform: "uppercase" }}>
+                GDrive PDF’s Summarizer | Python | LangChain | OpenAI | Pinecone | Streamlit UI
+                </h1>
+              </a>
+            </div>
+            <div className="exp-header-right">
+              <div className="experience-company-img">
+                <img src="music_store.png" alt="img" />
+              </div>
+            </div>
+          </div>
+          {/************** */}
+          <div className="experience-info">
+            <ul style={{ listStyle: "none", padding: 0 }}>
+              <li className="list-item">
+                ➔ Developed LangChain PDF Summarizer application with Streamlit UI, PyPDF2 for text extraction, and chunking
+for efficient handling of PDF content from Google Drive
+              </li>
+              <li className="list-item">
+                ➔ Integrated OpenAIEmbeddings for semantic embeddings, Pinecone as vector DB, and ChatOpenAI language
+model to deliver contextually relevant responses, enhancing user interactions and query-based summaries
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="experience-content-right d-none d-sm-block">
+          <div className="experience-company-img">
+            <img src="music_store.png" alt="img" />
+          </div>
+        </div>
+      </div>
+      {/*** new project block */}
+      <div className="experience-content-block">
+        <div className="experience-content-left">
+          <div className="experience-heading d-none d-sm-block">
+            {/* <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/SnehashisWalk/gdrive-pdfs-summarizer"
+            > */}
+              <h1 style={{ textTransform: "uppercase" }}>
+              MeetRecap - Condensing Zoom calls for insights | OpenAI | Pinecone | Streamlit UI | Airflow
+              </h1>
+            {/* </a> */}
+          </div>
+          {/* projects heading for sm devices */}
+          <div className="exp-header sm-toggle">
+            <div className="exp-header-left">
+              {/* <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/SnehashisWalk/gdrive-pdfs-summarizer"
+              > */}
+                <h1 style={{ textTransform: "uppercase" }}>
+                MeetRecap - Condensing Zoom calls for insights | OpenAI | Pinecone | Streamlit UI | Airflow
+                </h1>
+              {/* </a> */}
+            </div>
+            <div className="exp-header-right">
+              <div className="experience-company-img">
+                <img src="music_store.png" alt="img" />
+              </div>
+            </div>
+          </div>
+          {/************** */}
+          <div className="experience-info">
+            <ul style={{ listStyle: "none", padding: 0 }}>
+              <li className="list-item">
+                ➔ Streamlined audio transcription using Whisper API, GPT 3.5 APIs, and Pinecone vector DB for semantic search
+              </li>
+              <li className="list-item">
+                ➔ Designed a secure, scalable backend with FastAPI, authentication, and rate limiting, ensuring controlled access
+              </li>
+              <li className="list-item">
+                ➔ Containerized the application with Docker for easy deployment and scalability, and utilized Airflow for workflow
+orchestration
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="experience-content-right d-none d-sm-block">
+          <div className="experience-company-img">
+            <img src="music_store.png" alt="img" />
+          </div>
+        </div>
+      </div>
+      {/*** new project block */}
+      <div className="experience-content-block">
+        <div className="experience-content-left">
+          <div className="experience-heading d-none d-sm-block">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/SnehashisWalk/leetify"
+            > 
+              <h1 style={{ textTransform: "uppercase" }}>
+              LEETIFY - LEETCODE CLONE | React.js | Node.js | Redux | MongoDB | OpenAI
+              </h1>
+            </a>
+          </div>
+          {/* projects heading for sm devices */}
+          <div className="exp-header sm-toggle">
+            <div className="exp-header-left">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/SnehashisWalk/leetify"
+              > 
+                <h1 style={{ textTransform: "uppercase" }}>
+                LEETIFY - LEETCODE CLONE | React.js | Node.js | Redux | MongoDB | OpenAI
+                </h1>
+              </a>
+            </div>
+            <div className="exp-header-right">
+              <div className="experience-company-img">
+                <img src="music_store.png" alt="img" />
+              </div>
+            </div>
+          </div>
+          {/************** */}
+          <div className="experience-info">
+            <ul style={{ listStyle: "none", padding: 0 }}>
+              <li className="list-item">
+                ➔ Built a Leetcode clone, a web app for coding problems, used React.js and Redux for the UI and state
+management, Node.js as the backend, MongoDB as the database and OpenAI’s API to calculate time complexity
+              </li>
+              <li className="list-item">
+                ➔ Used Monaco-Editor for the code editor, accomplished code compilation and code execution in the backend
+utilizing Node.js streams, and Promises to execute the code across all test cases before sending the response
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="experience-content-right d-none d-sm-block">
+          <div className="experience-company-img">
+            <img src="music_store.png" alt="img" />
+          </div>
+        </div>
+      </div>
+      {/* --- new project block */}
       <div className="experience-content-block">
         <div className="experience-content-left">
           <div className="experience-heading d-none d-sm-block">
@@ -56,6 +218,7 @@ const Projects = () => {
           </div>
         </div>
       </div>
+      {/* --- new project block */}
       <div className="experience-content-block">
         <div className="experience-content-left">
           <div className="experience-heading d-none d-sm-block">
